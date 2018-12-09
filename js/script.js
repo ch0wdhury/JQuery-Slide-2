dbbfb6e3f817290e318b6a53d9b2fb84f867428d
Ashiq Ali Chowdhury <43188066+ch0wdhury@users.noreply.github.com>
2018-12-10 03:51:00 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,56 @@
+	$(document).ready(function(){
+		$("#btn").click(function(){
+			$("#btn").fadeOut(3000);
+		});
+
+		$(".cls").fadeTo(3000,0.3, function(){
+			alert("Got it!");
+		});
+
+		$("h2").click(function(){
+			$("p").fadeToggle(1000);
+
+		});
+
+		$("p").hide();
+
+
+		$("#btn1").click(function(){
+			$("#text").toggle(3000);
+
+		});
+
+		$("#btn2").click(function(){
+			$("#text").fadeOut(2000);
+
+		});
+
+		$("#btn3").click(function(){
+			$("#text").fadeIn(2000);
+
+		});
+
+		$("#btn4").click(function(){
+			$("#text").fadeToggle(2000);
+
+		});
+		$("#btn5").click(function(){
+			$("#text").fadeTo(2000,0.9);
+
+		});
+		$("#flip").click(function(){
+			$("#panel").slideToggle("slow");
+		});
+
+		$("li:odd").addClass("odd");
+		$("li:even").addClass("even");
+		$("li:first").addClass("first");
+		$("li:last").addClass("last");
+		$("li:eq(3)").addClass("nth");
+
+		$("p").click(function(){
+			$(this).hide("fast");
+		});
+
+		$("h1:not(h1:eq(5))").fadeOut(3000);
+	});
